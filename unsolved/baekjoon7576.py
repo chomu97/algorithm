142d9434c0a0879902528eb94b0ef689360869e9
chomu97 <ach1219@naver.com>
2022-11-23 00:14:10 +0900

    modify: solve problem

--- unsolved/baekjoon7576.py
+++ unsolved/baekjoon7576.py
@@ -1,67 +1,26 @@
-from queue import PriorityQueue
-col,row=map(int,input().split(' '))
-box=[]
-checkbox=[]
-queue=PriorityQueue()
-temp=PriorityQueue()
-for i in range(row):
+from collections import deque
+N, M = map(int, input().split())
+box = []
+tomatoes = deque()
+for row in range(M):
     box.append([])
-    checkbox.append([])
-    tomato_row=list(map(int, input().split(' ')))
-    for j in range(col):
-        box[i].append(tomato_row[j])
-        if tomato_row[j]==1 or tomato_row[j]==0:
-            checkbox[i].append(0)
-        else:
-            checkbox[i].append(1)
-        if tomato_row[j]==1:
-            queue.put((i,j))
-cnt=0
-while True:
-    if queue.empty() and temp.empty():
-        cnt-=1
-        if any(0 in line for line in checkbox):
-            cnt=-1
-        break
-    if queue.empty():
-        cnt += 1
-        queue=temp
-        temp=PriorityQueue()
-    start=queue.get()
-    x,y=start[0],start[1]
-    print(x,y,queue.queue)
-    if checkbox[x][y] or box[x][y]==-1:
-        continue
-    else:
-        if x==0: # first line
-            if not checkbox[x+1][y]:
-                temp.put((x+1,y))
-                box[x][y]=cnt
-        elif x==row-1: # last line
-            if not checkbox[x - 1][y]:
-                temp.put((x-1,y))
-                box[x-1][y]=cnt
-        else: # middle line
-            if not checkbox[x - 1][y]:
-                temp.put((x-1,y))
-                box[x - 1][y] = cnt
-            if not checkbox[x + 1][y]:
-                temp.put((x+1,y))
-                box[x+1][y]=cnt
-        if y==0: # first line
-            if not checkbox[x][y+1]:
-                temp.put((x,y+1))
-                box[x][y+1]=cnt
-        elif y==col-1: # last line
-            if not checkbox[x][y-1]:
-                temp.put((x,y-1))
-                box[x][y-1]=cnt
-        else: # middle line
-            if not checkbox[x][y-1]:
-                temp.put((x,y-1))
-                box[x][y - 1] = cnt
-            if not checkbox[x][y+1]:
-                temp.put((x,y+1))
-                box[x][y+1]=cnt
-    checkbox[x][y] = 1
-print(cnt)
+    for col, t in enumerate(list(map(int, input().split()))):
+        box[row].append(t)
+        if t == 1:
+            tomatoes.append((row, col))
+d = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+while tomatoes:
+    x, y = tomatoes.popleft()
+    for dx, dy in d:
+        nrow = x + dx
+        ncol = y + dy
+        if (0 <= nrow <= M-1) and (0<= ncol <= N-1):
+            if box[nrow][ncol] == 0:
+                box[nrow][ncol] = box[x][y] + 1
+                tomatoes.append((nrow, ncol))
+            elif box[nrow][ncol] >= 0:
+                box[nrow][ncol] = min(box[x][y] + 1, box[nrow][ncol])
+if any(0 in b for b in box):
+    print(-1)
+else:
+    print(max(max(b) for b in box) - 1)
