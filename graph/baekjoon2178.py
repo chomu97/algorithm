8ca6bece8edd208738ba7ea39ff4c974138de91a
chomu97 <ach1219@naver.com>
2022-11-22 22:17:08 +0900

    modify: solve problem

--- graph/baekjoon2178.py
+++ graph/baekjoon2178.py
@@ -2,24 +2,24 @@ def bfs(board, s):
     queue = [s]
     board[s[0]][s[1]] = 0
     diff = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-    discovered = []
-    cnt = 0
+    result = [[0]*len(board[0]) for _ in range(len(board))]
+    result[s[0]][s[1]] = 1
     while queue:
         new = queue.pop(0)
         if (N-1, M-1) in queue:
             break
-        cnt += 1
         for drow, dcol in diff:
             row, col = new
             row += drow
             col += dcol
             if (0 <= row <= N-1) and (0 <= col <= M-1):
-                if board[row][col] != 0 and (row, col) not in discovered:
+                if result[row][col] != 0:
+                    result[row][col] = min(result[row][col], result[row-drow][col-dcol] + 1)
+                if board[row][col] and result[row][col] == 0:
                     queue.append((row, col))
-                    discovered.append((row, col))
                     board[row][col] = 0
-        print(new)
-    return cnt
+                    result[row][col] = result[row-drow][col-dcol] + 1
+    return result[N-1][M-1]
 
 
 N, M = map(int, input().split())
