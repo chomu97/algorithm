7ddad22dc8967f34dd4e140a21976facf2bd97aa
chomu97 <ach1219@naver.com>
2023-04-12 21:30:13 +0900

    add comment

--- string/baekjoon9935.py
+++ string/baekjoon9935.py
@@ -16,3 +16,6 @@ else:
         else:
             stack.append(word)
     print('FRULA' if not stack else ''.join(stack))
+
+# bomb을 리스트에 넣으면 굳이 stack을 join시키지 않아도 됨.
+# 9line 다음에서, 무조건 stack.append(word)를 한 다음에 11line 비교 구문을 stack[-bomb_length:] == bomb 하면 더 좋을 듯.
